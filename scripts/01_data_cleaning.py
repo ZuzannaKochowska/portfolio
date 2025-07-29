4082cf2114cb8549d796f7e8fd99842696130ad0
Zuzanna Kochowska <zuzannakochowska@gmail.com>
2025-07-29 13:50:01 +0200

    check

--- scripts/01_data_cleaning.py
+++ scripts/01_data_cleaning.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 column_names = ['Finacial Rate', 'Amount in PLN']
 
-df = pd.read_csv('../zabka.csv', header=None, names=column_names)
+df = pd.read_csv('"C:\Users\Zuza\PycharmProjects\profitability analysis\kruk.csv"', header=None, names=column_names)
 df['Amount in PLN'] = df['Amount in PLN'].str.replace(' ', '', regex=False).str.replace(',', '.', regex=False).astype(float)
 
 percent_keywords = ['ROE', 'ROA', 'wskaźnik']
 mask = ~df['Finacial Rate'].str.lower().str.contains('|'.join([k.lower() for k in percent_keywords]))
 df.loc[mask, 'Amount in PLN'] = df.loc[mask, 'Amount in PLN'] * 1000
-df['Company'] = 'Żabka'
-df.to_csv("zabka_clean.csv", index=False)
+df['Company'] = 'Kruk'
+df.to_csv("kruk_clean.csv", index=False)
+
 
